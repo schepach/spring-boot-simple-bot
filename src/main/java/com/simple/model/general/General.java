f8b82d51ce56b46e7d7e130563958e30fa3249e3
schepach <welp.new@gmail.com>
2022-07-01 00:19:16 +0300

    Now using ApplicationContext for getting bot instance
    Because it's good approach. Application use singleton instance instead of use new instance declaration every time.

--- src/main/java/com/simple/model/general/General.java
+++ src/main/java/com/simple/model/general/General.java
@@ -1,15 +1,20 @@
 package com.simple.model.general;
 
 import com.simple.bot.SpringBootBot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class General implements IGeneral {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Override
     public void sayHello(String userId) {
         try {
-            new SpringBootBot().execute(SendMessage.builder()
+            applicationContext.getBean("springBootBot", SpringBootBot.class).execute(SendMessage.builder()
                     .chatId(userId)
                     .text("Привет!🤖\n/ping")
                     .build());
@@ -21,7 +26,7 @@ public class General implements IGeneral {
     @Override
     public void ping(String userId) {
         try {
-            new SpringBootBot().execute(SendMessage.builder()
+            applicationContext.getBean("springBootBot", SpringBootBot.class).execute(SendMessage.builder()
                     .chatId(userId)
                     .text("pong🏓")
                     .build());
